248aeb18a45a7b389b7a172d2c9cb0cf7b81aa47
Holly Chaffee <saltdc95@gmail.com>
2022-04-01 16:11:26 -0400

    first commit

---
+++ src/SavingsAccount.java
@@ -0,0 +1,51 @@
+public class SavingsAccount {
+    int balance;
+
+  public SavingsAccount(int initialBalance){
+    balance = initialBalance;
+  }
+
+  public void checkBalance() {
+    System.out.println("Hello! Your balance is " + balance);
+  }
+
+  public void deposit(int amountToDeposit) {
+    int newBalance = balance + amountToDeposit;
+    balance = newBalance;
+    System.out.println("You just deposited " + amountToDeposit);
+  }
+
+  public int withdraw(int amountToWithdraw) {
+    int newBalance = balance - amountToWithdraw;
+   balance = newBalance;
+    System.out.println("You just withdrew " + amountToWithdraw);
+    return amountToWithdraw;
+  }
+
+  public static void main(String[] args){
+    SavingsAccount savings = new SavingsAccount(2000);
+
+    //Check balance:
+    savings.checkBalance();
+
+    //Withdrawing:
+    savings.withdraw(250);
+
+    //Check balance:
+    savings.checkBalance();
+
+    //Deposit:
+    savings.deposit(600);
+
+    //Check balance:
+    savings.checkBalance();
+
+    //Deposit:
+    savings.deposit(700);
+
+    //Check balance:
+   savings.checkBalance();
+
+  }
+
+}
